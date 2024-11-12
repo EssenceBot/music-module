bdd809986547f1846ce65a101b56aaed046e5362
Faterek <seweryn.fater@proton.me>
2024-11-12 21:21:43 +0100

    refactor: replace damonjs with rainlink and reorganize command files

--- commands/play.ts
+++ commands/play.ts
@@ -1,46 +1,81 @@
-import { SearchResultTypes } from "damonjs/dist/Modules/Interfaces";
-import { ChannelType, type CommandInteraction, type GuildMember, type SlashCommandBuilder } from "discord.js";
+import {
+  ChannelType,
+  type CommandInteraction,
+  type GuildMember,
+  type SlashCommandBuilder,
+} from "discord.js";
 
-import { damonjs } from ".."
+import { rainlink } from "..";
 
 export const playSlashCommandHandler = (slashCommand: SlashCommandBuilder) => {
-    slashCommand.setName('play')
-        .setDescription('Add a track to queue and play it')
-        .addStringOption(option => option.setName('query').setDescription('Search query or URL').setRequired(true))
+  slashCommand
+    .setName("play")
+    .setDescription("Add a track to queue and play it")
+    .addStringOption((option) =>
+      option
+        .setName("query")
+        .setDescription("Search query or URL")
+        .setRequired(true)
+    );
 };
 
-export const playInteractionHandler = async (interaction: CommandInteraction) => {
-    const query = interaction.options.get('query')?.value as string;
-    if (interaction.channel?.type === ChannelType.GuildText) {
-        const voiceChannel = (interaction.member as GuildMember)?.voice.channel;
-        if (voiceChannel) {
-            const player = await damonjs.createPlayer({
-                guildId: interaction.guildId as string,
-                textId: interaction.channel?.id,
-                voiceId: voiceChannel.id,
-                volume: 40,
-                shardId: interaction.guild?.shardId,
-            });
-            const result = await player.search(query, { requester: interaction.user });
-            if (!result.tracks.length) {
-                await interaction.reply({ content: 'No tracks found', ephemeral: true });
-                await Bun.sleep(5000).then(() => interaction.deleteReply());
-                return;
-            }
-            if (result.type === SearchResultTypes.Playlist) player.queue.add(result.tracks);
-            else player.queue.add(result.tracks[0]);
-
-            if (!player.playable) await player.play(); // only use player.playable to play because if you use player.playing you can get unexpected bugs
-            if (player.paused) await player.pause(!player.paused); // if player is paused its gonna play
-            await interaction.reply(`Queued ${result.tracks[0].title}`);
-            await Bun.sleep(3000).then(() => interaction.deleteReply());
-            return;
-        } else {
-            await interaction.reply({ content: 'You need to be in a voice channel', ephemeral: true });
-            await Bun.sleep(5000).then(() => interaction.deleteReply());
-        }
-    } else {
-        await interaction.reply({ content: 'This command can only be used in a server', ephemeral: true });
+export const playInteractionHandler = async (
+  interaction: CommandInteraction
+) => {
+  const query = interaction.options.get("query")?.value as string;
+  console.log(`Query: ${query}`);
+  if (interaction.channel?.type === ChannelType.GuildText) {
+    const voiceChannel = (interaction.member as GuildMember)?.voice.channel;
+    console.log(`Voice channel: ${voiceChannel}`);
+    if (voiceChannel) {
+      const player = await rainlink.create({
+        guildId: interaction.guildId as string,
+        textId: interaction.channel?.id,
+        voiceId: voiceChannel.id,
+        shardId: interaction.guild?.shardId as number,
+        volume: 40,
+      });
+      const result = await rainlink.search(query, {
+        requester: interaction.user,
+      });
+      if (!result.tracks.length) {
+        console.log("No tracks found");
+        await interaction.reply({
+          content: "No tracks found",
+          ephemeral: true,
+        });
         await Bun.sleep(5000).then(() => interaction.deleteReply());
+        return;
+      }
+      if (result.type === "PLAYLIST")
+        for (let track of result.tracks) player.queue.add(track);
+      else player.queue.add(result.tracks[0]);
+
+      if (!player.playing || !player.paused) player.play();
+      console.log(
+        `Queued ${result.type === "PLAYLIST" ? result.tracks.length : 1} tracks`
+      );
+      await interaction.reply({
+        content:
+          result.type === "PLAYLIST"
+            ? `Queued ${result.tracks.length} from ${result.playlistName}`
+            : `Queued ${result.tracks[0].title}`,
+      });
+      await Bun.sleep(3000).then(() => interaction.deleteReply());
+      return;
+    } else {
+      console.log("User is not in a voice channel");
+      await interaction.reply({
+        content: "You need to be in a voice channel",
+        ephemeral: true,
+      });
+      await Bun.sleep(5000).then(() => interaction.deleteReply());
     }
-};   
+  } else {
+    await interaction.reply({
+      content: "This command can only be used in a server",
+      ephemeral: true,
+    });
+    await Bun.sleep(5000).then(() => interaction.deleteReply());
+  }
+};
--- index.ts
+++ index.ts
@@ -1,98 +1,105 @@
-import { Events, DamonJs, Plugins } from "damonjs";
-import { Shoukaku, Connectors } from 'shoukaku';
-import { createSlashCommand, getClient } from "@essence-discord-bot/api/botExtension";
+import { Rainlink, Library } from "rainlink";
+import {
+  createSlashCommand,
+  getClient,
+} from "@essence-discord-bot/api/botExtension";
 
-import { playSlashCommandHandler, playInteractionHandler } from "./commands/play";
-import { skipSlashCommandHandler, skipInteractionHandler } from "./commands/skip";
-import { previousSlashCommandHandler, previousInteractionHandler } from "./commands/previous";
-import { pauseSlashCommandHandler, pauseInteractionHandler } from "./commands/pause";
-import { loopSlashCommandHandler, loopInteractionHandler } from "./commands/loop";
-import { shuffleInteractionHandler, shuffleSlashCommandHandler } from "./commands/shuffle";
-import { clearInteractionHandler, clearSlashCommandHandler } from "./commands/clear";
- 
-
+import {
+  playSlashCommandHandler,
+  playInteractionHandler,
+} from "./commands/play.ts";
+// import {
+//   skipSlashCommandHandler,
+//   skipInteractionHandler,
+// } from "./commands/skip.ts";
+// import {
+//   previousSlashCommandHandler,
+//   previousInteractionHandler,
+// } from "./commands/previous.ts";
+// import {
+//   pauseSlashCommandHandler,
+//   pauseInteractionHandler,
+// } from "./commands/pause.ts";
+// import {
+//   loopSlashCommandHandler,
+//   loopInteractionHandler,
+// } from "./commands/loop.ts";
+// import {
+//   shuffleInteractionHandler,
+//   shuffleSlashCommandHandler,
+// } from "./commands/shuffle.ts";
+// import {
+//   clearInteractionHandler,
+//   clearSlashCommandHandler,
+// } from "./commands/clear.ts";
 
 const client = getClient();
 
-if (!process.env.LAVALINK_HOST) throw new Error("LAVALINK_HOST is not defined")
-    if (process.env.LAVALINK_SECURE === undefined) process.env.LAVALINK_SECURE = "false"
-    if (process.env.LAVALINK_SECURE !== "true" && process.env.LAVALINK_SECURE !== "false") throw new Error("LAVALINK_SECURE has wrong value")
+if (!process.env.LAVALINK_HOST) throw new Error("LAVALINK_HOST is not defined");
+if (process.env.LAVALINK_SECURE === undefined)
+  process.env.LAVALINK_SECURE = "false";
+if (
+  process.env.LAVALINK_SECURE !== "true" &&
+  process.env.LAVALINK_SECURE !== "false"
+)
+  throw new Error("LAVALINK_SECURE has wrong value");
+switch (process.env.LAVALINK_DRIVER?.toLowerCase()) {
+  case "lavalinkv4" || "lavalink/v4/koinu" || "lavalink":
+    process.env.LAVALINK_DRIVER = "lavalink/v4/koinu";
+    break;
+  case "lavalinkv3" || "lavalink/v3/koto":
+    process.env.LAVALINK_DRIVER = "lavalink/v3/koto";
+    break;
+  case "nodelink" || "nodelinkv2" || "nodelink/v2/nari":
+    process.env.LAVALINK_DRIVER = "nodelink/v2/nari";
+    break;
+  default:
+    process.env.LAVALINK_DRIVER = "lavalink/v4/koinu";
+    break;
+}
 
-    const Nodes = [
-        {
-            name: process.env.LAVALINK_NAME || 'default',
-            url: `${process.env.LAVALINK_HOST as string}:${process.env.LAVALINK_PORT || 2333}`,
-            auth: process.env.LAVALINK_PASSWORD || '',
-            secure: process.env.LAVALINK_SECURE as unknown as boolean,
-        },
-    ];
+const Nodes = [
+  {
+    name: process.env.LAVALINK_NAME || "default",
+    host: `${process.env.LAVALINK_HOST as string}`,
+    port: process.env.LAVALINK_PORT as unknown as number,
+    auth: process.env.LAVALINK_PASSWORD || "",
+    secure: process.env.LAVALINK_SECURE as unknown as boolean,
+    driver: process.env.LAVALINK_DRIVER as string,
+  },
+];
 
-export const damonjs = new DamonJs({
-    resolveError: {
-        max: 5,
-        time: 30 * 1000,
-    },
-    exceptions: {
-        max: 5,
-        time: 30 * 1000,
-    },
-    stuck: {
-        max: 5,
-        time: 30 * 1000,
-    },
-    skipResolveError: true,
-    skipOnException: true,
-    skipOnStuck: true,
-    defaultSearchEngine: 'spotify',
-    defaultYoutubeThumbnail: 'default',
-
-    plugins: [new Plugins.PlayerMoved(client)],
-},
-new Shoukaku(new Connectors.DiscordJS(client), Nodes, {
-    moveOnDisconnect: false,
-    resume: false,
-    resumeTimeout: 30,
-    reconnectTries: 2,
-    restTimeout: 10000,
-}),
-);
+export const rainlink = new Rainlink({
+  library: new Library.DiscordJS(client),
+  nodes: Nodes,
+});
 
 export function discordBotInit() {
-    damonjs.shoukaku.on('ready', (name) => console.log(`[Music module] Lavalink ${name}: Ready!`));
-    damonjs.shoukaku.on('error', (name, error) => console.error(`[Music module] Lavalink ${name}: Error Caught,`, error));
-    damonjs.shoukaku.on('close', (name, code, reason) =>
-        console.warn(`[Music module] Lavalink ${name}: Closed, Code ${code}, Reason ${reason || 'No reason'}`),
+  rainlink.on("nodeConnect", (node) =>
+    console.log(`Lavalink ${node.options.name}: Ready!`)
+  );
+  rainlink.on("nodeError", (node, error) =>
+    console.error(`Lavalink ${node.options.name}: Error Caught,`, error)
+  );
+  rainlink.on("nodeClosed", (node) =>
+    console.warn(`Lavalink ${node.options.name}: Closed`)
+  );
+  // rainlink.on('debug', (name, info) => console.debug(`Lavalink ${name}: Debug,`, info));
+  rainlink.on("nodeDisconnect", (node, code, reason) => {
+    console.warn(
+      `Lavalink ${node.options.name}: Disconnected, Code ${code}, Reason ${
+        reason || "No reason"
+      }`
     );
-
-    damonjs.on(Events.PlayerCreate, async (player) => {
-        const channel = client.channels.cache.get(player.textId) || (await client.channels.fetch(player.textId).catch(() => null));
-        if (channel && channel.isTextBased()) {
-            const message = await channel.send({ content: 'Created a player' });
-            player.data.set('message', message);
-        }
-    });
-    damonjs.on(Events.PlayerStart, async (player, track) => {
-        const message = player.data.get('message');
-        message && (await message.edit({ content: `Started playing ${track.title}` }));
-    });
-    damonjs.on(Events.PlayerEnd, async (player) => {
-        const message = player.data.get('message');
-        message && (await message.edit({ content: `Finished playing` }));
-    });
-
-    damonjs.on(Events.PlayerEmpty, async (player) => {
-        const message = player.data.get('message');
-        message && (await message.edit({ content: `Destroyed player due to inactivity.` }));
-        player.destroy();
-        await Bun.sleep(3000).then(() => message?.delete());
-    });
-    
-    createSlashCommand(playSlashCommandHandler, playInteractionHandler)
-    createSlashCommand(skipSlashCommandHandler, skipInteractionHandler);
-    createSlashCommand(previousSlashCommandHandler, previousInteractionHandler);
-    createSlashCommand(pauseSlashCommandHandler, pauseInteractionHandler);
-    createSlashCommand(loopSlashCommandHandler, loopInteractionHandler);
-    createSlashCommand(shuffleSlashCommandHandler, shuffleInteractionHandler)
-    createSlashCommand(clearSlashCommandHandler, clearInteractionHandler);
-    console.log('[Music module] Registered 7 slash commands: [play, skip, previous, pause, loop, shuffle, clear]')
+  });
+  createSlashCommand(playSlashCommandHandler, playInteractionHandler);
+  //   createSlashCommand(skipSlashCommandHandler, skipInteractionHandler);
+  //   createSlashCommand(previousSlashCommandHandler, previousInteractionHandler);
+  //   createSlashCommand(pauseSlashCommandHandler, pauseInteractionHandler);
+  //   createSlashCommand(loopSlashCommandHandler, loopInteractionHandler);
+  //   createSlashCommand(shuffleSlashCommandHandler, shuffleInteractionHandler);
+  //   createSlashCommand(clearSlashCommandHandler, clearInteractionHandler);
+  console.log(
+    "[Music module] Registered 7 slash commands: [play, skip, previous, pause, loop, shuffle, clear]"
+  );
 }
